dd5b9a922ca8a0a11bf871463618882f1db3e7a1
ZahraHabibi <habibi.zahra.ku@gmail.com>
2022-10-15 10:01:45 -0700

    main controller added

---
+++ Final_Wave/Controllers/MainSiteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Wave.Controllers
+{
+    public class MainSiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
