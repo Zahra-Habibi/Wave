87379868f59501aad84fced7d5614107f017803b
ZahraHabibi <habibi.zahra.ku@gmail.com>
2022-10-17 10:47:59 -0700

    servicedetials component page added

---
+++ Final_Wave/ViewComponents/ServiceDetialsComponent/ServiceDetialsComponent.cs
@@ -0,0 +1,18 @@
+﻿using Final_Wave.DataLayer.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Wave.ViewComponents.ServiceDetialsComponent
+{
+    public class ServiceDetialsComponent :ViewComponent
+    {
+        private IUnitOfWork _temp;
+        public ServiceDetialsComponent(IUnitOfWork temp)
+        {
+            _temp = temp;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            return await Task.FromResult((IViewComponentResult)View("MyServieDetialView", await _temp.serviceUW.GetByIdAsync(id)));
+        }
+    }
+}
