d61794dfa22dcc575f129a7f6fa5a6b1e2f70bf5
ZahraHabibi <habibi.zahra.ku@gmail.com>
2022-10-30 12:02:21 -0700

    chat hub added

---
+++ Final_Wave/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Final_Wave.Hubs
+{
+    [Authorize]
+    public class ChatHub :Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            return base.OnConnectedAsync();
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task SendMessageToGroup(string sender, string receiver, string message)
+        {
+            return Clients.Group(receiver).SendAsync("ReceiveMessage", sender, message);
+        }
+    }
+}
