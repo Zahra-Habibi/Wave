d2f0b20e368c783b5ad234d3facbf282568e55ab
ZahraHabibi <habibi.zahra.ku@gmail.com>
2022-11-08 09:37:30 -0800

    chathub done

--- Final_Wave/Hubs/ChatHub.cs
+++ Final_Wave/Hubs/ChatHub.cs
@@ -6,19 +6,36 @@ namespace Final_Wave.Hubs
     [Authorize]
     public class ChatHub :Hub
     {
+        //total views in window
+        public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+
         public override Task OnConnectedAsync()
         {
-            Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
             return base.OnConnectedAsync();
         }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            TotalUsers --;
+            Clients.All.SendAsync("updateTotalUsrs", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            await Clients.All.SendAsync("updateTotalViews",TotalViews);
+        }
+        //public chat
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        public Task SendMessageToGroup(string sender, string receiver, string message)
-        {
-            return Clients.Group(receiver).SendAsync("ReceiveMessage", sender, message);
-        }
+     
     }
 }
