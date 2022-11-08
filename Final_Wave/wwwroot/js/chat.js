ae0c05ff2adaaf424f8c14cdf6e91034cfb43eed
ZahraHabibi <habibi.zahra.ku@gmail.com>
2022-11-08 08:59:43 -0800

    chat.js done

--- Final_Wave/wwwroot/js/chat.js
+++ Final_Wave/wwwroot/js/chat.js
@@ -1,33 +1,29 @@
-﻿"use strict";
-
+﻿
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-//Disable send button until connection is established
-document.getElementById("#sendButton").disabled = true;
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
-    //var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    //var encodedMsg = user + " says " + msg;
     var li = document.createElement("li");
-    /*li.textContent = encodedMsg;*/
-    document.getElementById("#messagesList").appendChild(li);
-    li.textContent = '${user} says ${message}';
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
 });
 
 connection.start().then(function () {
-    document.getElementById("#sendButton").disabled = false;
+    document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-document.getElementById("#sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("#senderInput").value;
-    var receiver = document.getElementById("#receiverInput").value;
-    var message = document.getElementById("#messageInput").value;
-
-
-    connection.invoke("SendMessageToGroup", user, receiver, message).catch(function (err) {
-            return console.error(err.toString());
-        });
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
     event.preventDefault();
 });
