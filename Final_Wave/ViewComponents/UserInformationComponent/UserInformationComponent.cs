b1b9a2b897a5bed8a402710cdf3dea2744f68708
ZahraHabibi <habibi.zahra.ku@gmail.com>
2023-02-16 08:04:21 -0800

    userinformationcomponent added

---
+++ Final_Wave/ViewComponents/UserInformationComponent/UserInformationComponent.cs
@@ -0,0 +1,20 @@
+﻿using Final_Wave.DataLayer.Entites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Wave.ViewComponents.UserInfoComponent
+{
+    public class UserInformationComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserInformationComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("UserInformation", await _userManager.GetUserAsync(HttpContext.User)));
+
+        }
+    }
+}
